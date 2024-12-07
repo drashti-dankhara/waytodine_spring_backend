14d1ff2c710bdd143307ad2df06d55cceeae446a
drashti-dankhara <drashti.dankhra@technoproject.in>
2024-12-07 08:58:14 +0530

    Initial commit3

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use a lightweight JDK base image
+FROM eclipse-temurin:17-jdk-alpine
+
+# Set the working directory inside the container
+WORKDIR /tmp
+
+# Copy the JAR file into the container
+COPY target/*.jar app.jar
+
+# Expose the port on which the application runs
+EXPOSE 8080
+
+# Command to run the application
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/com/waytodine/waytodine/controller/HelloController.java
+++ src/main/java/com/waytodine/waytodine/controller/HelloController.java
@@ -1,4 +1,20 @@
 package com.waytodine.waytodine.controller;
 
+import com.waytodine.waytodine.model.Category;
+import com.waytodine.waytodine.util.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class HelloController {
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello() {
+        return ResponseEntity.ok("hello");
+    }
 }
